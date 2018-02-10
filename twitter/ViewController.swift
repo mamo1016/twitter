05321318fdd839a9463779b7b9b5299772e94d1e
mamo1016 <rr0061vv@ed.ritsumei.ac.jp>
2018-02-10 13:05:18 +0900

    Initial Commit

---
+++ twitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter
+//
+//  Created by 上田　護 on 2018/02/10.
+//  Copyright © 2018年 上田　護. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
